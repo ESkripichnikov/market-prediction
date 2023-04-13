03220db5556ba43498cde94460c5947ddb3570d1
ESkripichnikov <eskripichnikov@gmail.com>
2023-04-13 12:29:45 +0300

    updated metric

--- utils/metric_clf_hft.py
+++ utils/metric_clf_hft.py
@@ -2,21 +2,18 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix
 
-def metric(y_true, y_pred, confusion=False):
+def metric(y_true, y_pred):
     conf_matrix = confusion_matrix(y_true, y_pred)
     weight_matrix = np.array([
-                [1.9, -0.3, -2],
-                [0,    0,    0],
-                [-2, -0.3, 1.9]
+        [1.9,     0,    -2],
+        [-0.3,    0,    -0.3],
+        [-2,      0,     1.9]
     ])
     
     hit_matrix = conf_matrix * weight_matrix
     hit_matrix_sum = np.sum(hit_matrix)
-    action_count = np.sum(conf_matrix[0]) + np.sum(conf_matrix[2])
-    
-    result = hit_matrix_sum / np.sqrt(action_count)
-    
-    if confusion:
-        return result, conf_matrix
+    action_count = np.sum(conf_matrix[:, 0]) + np.sum(conf_matrix[:, 2])
+    if action_count == 0:
+        return 0
     else:
-        return result
+        return hit_matrix_sum / np.sqrt(action_count)
